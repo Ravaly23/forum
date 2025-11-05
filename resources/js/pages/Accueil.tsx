cbe24c74339aa7a91c97dc41e166c8b88d71b995
Ravaly23 <valeryravaly2003@gmail.com>
2025-11-05 10:03:27 +0300

    Projet Etudiant maj

--- resources/js/pages/Accueil.tsx
+++ resources/js/pages/Accueil.tsx
@@ -1,11 +1,10 @@
-// import '@/components/header';
+
 import Header from '@/components/header';
-// import '@/components/Main';
 import Main from '@/components/Main';
 import { Head } from '@inertiajs/react';
-//test
+
 export default function Accueil(){
-    const test : string ="valery"
+
     return (
         <>
          <Head title='Fagnampy'/>
